c0704422fdf9295c8f1222c7c8ad9a62843a16c9
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-17 12:51:12 -0700

    Write tests for adding a pet to an application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe 'As a Visitor' do
   	@application_1 = Application.create!(name: 'Ben', street: '123 street', city: 'Denver', state: 'CO', zip: '80220')
     @shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
     @pet1 = @shelter1.pets.create!(image:"", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
+    @application_2 = Application.create!(name: 'Jammy', street: '123 street', city: 'Denver', state: 'CO', zip: '80220')
+    @application_2.pets << @pet1
+
   end
   describe'when I navigate to an application show page' do
 
   	it'displays the information about the application'do
   	visit "applications/#{@application_1.id}"
-
     	within("#shown-app") do
   			expect(page).to have_content("Ben")
   			expect(page).to have_content("123 street,Denver,CO,80220")
@@ -18,10 +20,34 @@ RSpec.describe 'As a Visitor' do
   	end
 
     it'can search for pets'do
-    visit "applications/#{@application_1.id}"
+      visit "applications/#{@application_1.id}"
       fill_in("search", with: "thor")
       click_button("Search")
-      expect(page).to have_content("name: #{@pet1.name}")
+      within("#in-progress-app")do
+        expect(page).to have_content("#{@pet1.name}")
+      end
+    end
+
+     it'can search for pets and add it to the application'do
+      visit "applications/#{@application_1.id}"
+      fill_in("search", with: "thor")
+      click_button("Search")
+      within("#in-progress-app")do
+        expect(page).to have_button("Adopt this pet")
+        click_button("Adopt this pet")
+      end
+
+      within("#attached-pets")do
+        expect(page).to have_content("#{@pet1.name}")
+      end
+      fill_in("description", with: "me want thor")
+      click_button("Submit Application")
+      
+
+      within("#shown-app")do
+        expect(page).to have_content("Application Status: Pending")
+      end
+      
     end
   end
 
