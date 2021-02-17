962fd546c7b3490c66320e66e58efd8cbdba6562
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-17 15:43:09 -0700

    Update models to reflect methods used for searching and manipulating attributes

--- app/models/application.rb
+++ app/models/application.rb
@@ -11,5 +11,24 @@ class Application < ApplicationRecord
 	def get_full_address
 		[street,city,state,zip].join(",")
 	end
+
+	def self.search(search)
+		if search
+			Pet.where("LOWER(name) like ?", "%#{search.downcase}%")
+		else
+			Pet.all
+		end  	
+  end
+
+  def add_pet(arg_id)
+  	matching = Pet.where("id = #{arg_id}")
+  	matching.each do |pet|
+  		pets << pet
+  	end
+  end
+
+  def submit(desc)
+  	assign_attributes(status: "Pending", description: desc)
+  end
 	
 end
