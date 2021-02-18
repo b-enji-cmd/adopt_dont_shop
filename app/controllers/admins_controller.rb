ebc0f158a4a774c7a2f339dd174b9b83942994cc
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-17 20:00:43 -0700

    Write admin controller actions

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,4 +2,15 @@ class AdminsController < ApplicationController
 	def show
 		@application = Application.find(params[:id])
 	end
+
+	def update
+		application = Application.find(params[:id])
+		pet = Pet.find(params[:pet_id])
+		pet.adopt
+		redirect_to "/admin/applications/#{application.id}"
+	end
+	private
+  def admin_params
+    params.permit(:pet_id)
+  end
 end
