7c6ebc16bc37e176797c136f4ba32ed3866959cf
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-17 17:37:06 -0700

    Write test for pets on app

--- spec/features/admins/show_spec.rb
+++ spec/features/admins/show_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe'As a visitor'do
 			expect(page).to have_content("#{@application_2.name}")
 		end
 	end
+
+	it'displays the pets on the application'do
+		visit "/admin/applications/#{@application_2.id}"
+		within("#app-info")do
+		expect(page).to have_content("Pets on application:")
+			within("#pet-#{@pet1.id}")do
+				expect(page).to have_content("#{@pet1.name}")
+			end
+		end
+	end
 end
